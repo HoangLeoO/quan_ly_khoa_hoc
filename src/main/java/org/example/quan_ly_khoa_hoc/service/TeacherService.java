20ecc5c467ebd4ef0d51b7ee89c4dee71c5249a3
HoangLeoO <leoo09012001@gmail.com>
2025-11-19 10:33:02 +0700

    oke

--- src/main/java/org/example/quan_ly_khoa_hoc/service/TeacherService.java
+++ src/main/java/org/example/quan_ly_khoa_hoc/service/TeacherService.java
@@ -1,6 +1,14 @@
 package org.example.quan_ly_khoa_hoc.service;
 
+import org.example.quan_ly_khoa_hoc.entity.Staff;
 import org.example.quan_ly_khoa_hoc.service.serviceInterface.ITeacherService;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class TeacherService implements ITeacherService {
+    @Override
+    public Staff addStaffInTransaction(Connection connection, Staff staff) throws SQLException {
+        return null;
+    }
 }
